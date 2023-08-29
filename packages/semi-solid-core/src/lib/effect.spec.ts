63cb3ed44b08fb1f9ae48d794024b82321a88773
Dustin Ewan <dustineewan@gmail.com>
2023-08-28 21:36:35 -0500

    add effects tests

--- packages/semi-solid-core/src/lib/effect.spec.ts
+++ packages/semi-solid-core/src/lib/effect.spec.ts
@@ -1,5 +1,17 @@
 import { createSignalEffect } from './effect';
+import { ResourceSignal, createResource } from './resource';
 import { createSignal } from './signal';
+
+async function waitUntilFinishedLoading(resource: ResourceSignal<unknown>) {
+  return new Promise((resolve) => {
+    const handle = setInterval(() => {
+      if (!resource.loading) {
+        clearInterval(handle);
+        resolve(null);
+      }
+    }, 200);
+  });
+}
 
 describe('effects', () => {
   it('should execute properly when signal updates', () => {
@@ -26,5 +38,42 @@ describe('effects', () => {
     setSignal(5);
 
     expect(effectValue).toEqual(5);
+  });
+
+  it('should execute when effect is monitoring a computed value', () => {
+    const [signal, setSignal] = createSignal(5);
+    let effectValue = 0;
+
+    const double = () => signal() * 2;
+
+    createSignalEffect(() => {
+      effectValue = double();
+    });
+
+    setSignal(6);
+
+    expect(effectValue).toEqual(12);
+  });
+
+  it('should execute when effect is monitoring a resource', async () => {
+    const source = createSignal('Bob');
+    const [, setSignal] = source;
+    let effectValue = 0;
+
+    const [nameLength] = createResource(source, (value: string) =>
+      Promise.resolve(value.length)
+    );
+
+    createSignalEffect(() => {
+      effectValue = `The secret person's name is ${nameLength()} characters long`;
+    });
+
+    setSignal('Alice');
+
+    await waitUntilFinishedLoading(nameLength);
+
+    expect(effectValue).toEqual(
+      `The secret person's name is 5 characters long`
+    );
   });
 });
